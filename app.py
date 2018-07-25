ee76cf62377e8c4314a7d0b45d93eea7dbc16f09
Jim Cockburn <jim.cockburn@skyscanner.net>
2018-07-25 06:16:09 +0100

    Make code pep8 compliant

--- app.py
+++ app.py
@@ -3,7 +3,11 @@ import string
 import random
 import httplib2
 from datetime import datetime
-from flask import Flask, request, render_template, session, make_response, jsonify, url_for, redirect
+from flask import (
+    Flask, request, render_template,
+    session, make_response, jsonify,
+    url_for, redirect
+)
 from sqlalchemy import create_engine, MetaData, asc, update
 from sqlalchemy.orm import sessionmaker
 from functools import wraps
@@ -51,7 +55,7 @@ def get_user_id(email):
     try:
         user = db_session.query(User).filter_by(email=email).one()
         return user.id
-    except:
+    except Exception:
         return None
 
 
@@ -59,7 +63,7 @@ def get_user_id_that_created_item(name):
     try:
         item = db_session.query(CatalogItem).filter_by(name=name).one()
         return item.user_id
-    except:
+    except Exception:
         return None
 
 
@@ -89,12 +93,12 @@ def fb_login():
     app_secret = json.loads(
         open('static/data/fb_client_secrets.json', 'r').read()
     )['web']['app_secret']
-    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id={}&client_secret={}&fb_exchange_token={}'.format(app_id, app_secret, access_token)
+    url = 'https://graph.facebook.com/oauth/access_token?grant_type=fb_exchange_token&client_id={}&client_secret={}&fb_exchange_token={}'.format(app_id, app_secret, access_token)  # noqa
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
     token = result.split(',')[0].split(':')[1].replace('"', '')
 
-    url = 'https://graph.facebook.com/v2.8/me?access_token={}&fields=name,id,email'.format(token)
+    url = 'https://graph.facebook.com/v2.8/me?access_token={}&fields=name,id,email'.format(token)  # noqa
     h = httplib2.Http()
     result = h.request(url, 'GET')[1]
     data = json.loads(result)
@@ -118,7 +122,7 @@ def fb_logout():
     user = session.get('user', {})
     facebook_id = user.get('facebook_id')
     access_token = user.get('access_token')
-    url = 'https://graph.facebook.com/{}/permissions?access_token={}'.format(facebook_id, access_token)
+    url = 'https://graph.facebook.com/{}/permissions?access_token={}'.format(facebook_id, access_token)  # noqa
     h = httplib2.Http()
     h.request(url, 'DELETE')
     session.pop('user', None)
@@ -130,9 +134,10 @@ def is_logged(endpoint_func):
     def check_is_logged(*args, **kwargs):
         if 'user' not in session:
             return "You need to log in first"
-        if 'item' in kwargs:
-            if get_user_id(session['user']["email"] != get_user_id_that_created_item(kwargs['item'])):
-                return "You are not the owner of the item"
+        user_id = get_user_id(session['user']["email"])
+        user_id_item = get_user_id_that_created_item(kwargs['item'])
+        if 'item' in kwargs and user_id != user_id_item:
+            return "You are not the owner of the item"
         return endpoint_func(*args, **kwargs)
     return check_is_logged
 
@@ -152,8 +157,17 @@ def home():
 @app.route('/<catagory>/items')
 def items_by_catagory(catagory):
     catagories = db_session.query(Catagory).order_by(asc(Catagory.name))
-    catagory_id = db_session.query(Catagory).filter(Catagory.name == catagory).one().id
-    items = db_session.query(CatalogItem).order_by(asc(CatalogItem.name)).filter(CatalogItem.catagory_id == catagory_id)
+    catagory_id = (
+        db_session.query(Catagory)
+        .filter(Catagory.name == catagory)
+        .one()
+        .id
+    )
+    items = (
+        db_session.query(CatalogItem)
+        .order_by(asc(CatalogItem.name))
+        .filter(CatalogItem.catagory_id == catagory_id)
+    )
     return render_template(
         'catagory_list.html',
         selected_catagory=catagory,
@@ -164,7 +178,12 @@ def items_by_catagory(catagory):
 
 @app.route('/catalog/<item_group>/<item>')
 def view_item(item_group, item):
-    selected_item = db_session.query(CatalogItem).filter(Catagory.name == item_group).filter(CatalogItem.name == item).one()
+    selected_item = (
+        db_session.query(CatalogItem)
+        .filter(Catagory.name == item_group)
+        .filter(CatalogItem.name == item)
+        .one()
+    )
     return render_template(
         'view_item.html',
         item=selected_item,
@@ -176,21 +195,33 @@ def view_item(item_group, item):
 @is_logged
 def edit_item(item):
     if request.method == 'POST':
-        catagory = db_session.query(Catagory).filter(Catagory.name == request.form['catagory']).first()
+        catagory = (
+            db_session.query(Catagory)
+            .filter(Catagory.name == request.form['catagory'])
+            .first()
+        )
         if not catagory:
             catagory = Catagory(
                 name=request.form['catagory']
             )
             db_session.add(catagory)
             db_session.commit()
-        item_to_update = db_session.query(CatalogItem).filter_by(id=request.form['id']).first()
+        item_to_update = (
+            db_session.query(CatalogItem)
+            .filter_by(id=request.form['id'])
+            .first()
+        )
         item_to_update.name = request.form['name']
         item_to_update.description = request.form['description']
         item_to_update.catagory_id = catagory.id
         db_session.commit()
         return redirect(url_for('home'))
     else:
-        selected_item = db_session.query(CatalogItem).filter(CatalogItem.name == item).one()
+        selected_item = (
+            db_session.query(CatalogItem)
+            .filter(CatalogItem.name == item)
+            .one()
+        )
         return render_template(
             'edit_item.html',
             item=selected_item
@@ -202,27 +233,53 @@ def edit_item(item):
 def delete_item(item):
     if request.method == 'POST':
         # there is still a small bug here
-        catagory_id = db_session.query(CatalogItem).filter(CatalogItem.id == request.form['id']).one().catagory_id
-        db_session.query(CatalogItem).filter(CatalogItem.id == request.form['id']).delete()
+        catagory_id = (
+            db_session.query(CatalogItem)
+            .filter(CatalogItem.id == request.form['id'])
+            .one()
+            .catagory_id
+        )
+        (
+            db_session.query(CatalogItem)
+            .filter(CatalogItem.id == request.form['id'])
+            .delete()
+        )
         db_session.commit()
         try:
-            db_session.query(CatalogItem).filter(CatalogItem.id == catagory_id).first()
-        except:
-            db_session.query(Catagory).filter(Catagory.id == catagory_id).delete()
+            (
+                db_session.query(CatalogItem)
+                .filter(CatalogItem.id == catagory_id)
+                .first()
+            )
+        except Exception:
+            (
+                db_session.query(Catagory)
+                .filter(Catagory.id == catagory_id)
+                .delete()
+            )
             db_session.commit()
         return redirect(url_for('home'))
     else:
-        selected_item = db_session.query(CatalogItem).filter(CatalogItem.name == item).one()
+        selected_item = (
+            db_session.query(CatalogItem)
+            .filter(CatalogItem.name == item)
+            .one()
+        )
         return render_template(
             'delete_item.html',
             item=selected_item
         )
 
+
 @app.route('/catalog/additem/', methods=['GET', 'POST'])
 @is_logged
 def add_item():
     if request.method == 'POST':
-        catagory = db_session.query(Catagory).filter(Catagory.name == request.form['catagory']).first()
+        catagory = (
+            db_session.query(Catagory)
+            .filter(Catagory.name == request.form['catagory'])
+            .first()
+        )
         if not catagory:
             catagory = Catagory(
                 name=request.form['catagory']
